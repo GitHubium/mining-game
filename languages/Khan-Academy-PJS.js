81fcd8d6851446047a6cc3f60cf2d3eacdc6667d
GitHubium <40664983+GitHubium@users.noreply.github.com>
2018-07-28 19:47:50 -0400

    Update Khan-Academy-PJS.js
    Changed collisions, added oh noes, added `down_keys`, created basic map generator.

--- languages/Khan-Academy-PJS.js
+++ languages/Khan-Academy-PJS.js
@@ -4,6 +4,7 @@ frameRate(framesPerSecond);
 noStroke();
 textAlign(CENTER, CENTER);
 rectMode(LEFT);
+imageMode(CENTER);
 
 /** Variables **/
 var vMap;
@@ -19,14 +20,14 @@ var cam = {// Camera variables
     y: 5,
     dragForceX: 0,// Used for the smooth effect when panning around the map
     dragForceY: 0,// ^
-    minHt: 0.01,// Inwards zoom limit
+    minHt: 0.002,// Inwards zoom limit
     maxHt: 1,// Outwards zoom limit
     hw: width/2,// hw = half width of canvas
     hh: height/2,// hh = half height of canvas
 };
 var uint8=(function(){return this.Uint8Array;})();///
-var vMapColumns = 20;
-var vMapRows; //determined after generateMap is called
+var vMapColumns = 40;
+var vMapRows = 40;
 
 
 var mapKeyColors = [
@@ -35,9 +36,10 @@ var mapKeyColors = [
     color(138, 50, 50),// lava
     color(34, 255, 0),// grass
     color(122, 79, 9),// earth
+    color(219, 219, 219),// white
     ];
     
-var gravity = 0.0055;/// maybe turn into physics.gravity
+var gravity = 0.008;/// maybe turn into physics.gravity
 var maxGravityOnObject = 0.4; // maximum fall speed of an object, because of "air friction"
 
 /** The key:
@@ -85,13 +87,13 @@ var cameraUpdate = function() {
         cam.dragForceY *= 0.7;
     }
 
-    /* UP/DOWN arrow key zoom logic *//*
-    if (keys[UP] && cam.gotoHt > cam.minHt) {
+    /* Q/A key zoom logic */
+    if (down_keys[81] && cam.gotoHt > cam.minHt) {
         cam.gotoHt *= 0.95;
-    } else if (keys[DOWN] && cam.gotoHt < cam.maxHt) {
+    } else if (down_keys[65] && cam.gotoHt < cam.maxHt) {
         cam.gotoHt /= 0.95;
     }
-*/
+
     /* Smooth zoom logic */
     if(cam.ht < cam.gotoHt/1.01){
         cam.ht += (cam.gotoHt-cam.ht)*0.4;
@@ -100,19 +102,113 @@ var cameraUpdate = function() {
     }else{cam.ht = cam.gotoHt;}
 };
 
+var collidelib = {
+    player_to_map:function(playerObj) {// check 4 nearest blocks for collisions
+    
+        var roundedX = round(playerObj.centerX);
+        var roundedY = round(playerObj.centerY);// rounded coors are also the BR block coors
+        
+        var blockTL = vMap[(roundedX-1)+vMapColumns*(roundedY-1)];// top-left block
+        var blockTR = vMap[(roundedX)+vMapColumns*(roundedY-1)];// top-right block
+        var blockBL = vMap[(roundedX-1)+vMapColumns*(roundedY)];// bottom-left block
+        var blockBR = vMap[(roundedX)+vMapColumns*(roundedY)];// bottom-right block
+        var _isFalling = true;
+        
+        if (blockTL) {// if top-left block is solid
+            if (playerObj.x1 < roundedX && playerObj.y1 < roundedY) {
+                if (abs(playerObj.x1-(roundedX-0.5)) > abs(playerObj.y1-(roundedY-0.5))) {
+                    playerObj.velX = 0;
+                    playerObj.x1 = roundedX;
+                    playerObj.configXs();
+                }
+                else {
+                    playerObj.velY = 0;
+                    playerObj.y1 = roundedY;
+                    playerObj.configYs();
+                }
+            }
+        }
+        if (blockTR) {// if top-right block is solid
+            if (playerObj.y1 < roundedY && playerObj.x2 > roundedX) {
+                if (abs(playerObj.x2-(roundedX+0.5)) > abs(playerObj.y1-(roundedY-0.5))) {
+                    playerObj.velX = 0;
+                    playerObj.x1 = roundedX-playerObj.w;
+                    playerObj.configXs();
+                } else  {
+                    playerObj.velY = 0;
+                    playerObj.y1 = roundedY;
+                    playerObj.configYs();
+                }
+            }
+        }
+        if (blockBL) {// if bottom-left block is solid
+            if (playerObj.x1 < roundedX && playerObj.y2 > roundedY) {
+                if (abs(playerObj.x1-(roundedX-0.5)) > abs(playerObj.y2-(roundedY+0.5))) { 
+                    playerObj.velX = 0;
+                    playerObj.x1 = roundedX;
+                    playerObj.configXs();
+                } else {
+                    playerObj.velY = 0;
+                    playerObj.y1 = roundedY-playerObj.h;
+                    playerObj.configYs();
+                    _isFalling = false;
+                    playerObj.isFalling = false;
+                }
+            }            
+        }
+        if (blockBR) {// if bottom-right block is solid
+            if (playerObj.x2 > roundedX && playerObj.y2 > roundedY) {
+                if (abs(playerObj.x2-(roundedX+0.5)) > abs(playerObj.y2-(roundedY+0.5))) { 
+                    playerObj.velX = 0;
+                    playerObj.x1 = roundedX-playerObj.w;
+                    playerObj.configXs();
+                } else {
+                    playerObj.velY = 0;
+                    playerObj.y1 = roundedY-playerObj.h;
+                    playerObj.configYs();
+                    _isFalling = false;
+                    playerObj.isFalling = false;
+                }        
+            }
+        }
+        
+        if (_isFalling) {
+            var blockBBL = vMap[(roundedX-1)+vMapColumns*(roundedY)];// below-left block
+            var blockBBR = vMap[(roundedX)+vMapColumns*(roundedY)];// below-right block
+            
+            if (!blockBBL) {
+                if (!blockBBR) {// both sides are empty
+                    playerObj.isFalling = true;
+                } else if (playerObj.x2 <= roundedX) {
+                    playerObj.isFalling = true;
+                }
+            } else {
+                if (playerObj.x1 >= roundedX) {
+                    playerObj.isFalling = true;
+                }
+            }
+        }
+
+        
+    }
+    
+    
+    
+};
+
 var generateMap = function() {
     
     var array = [];
-    for (var i = 0; i < 400; i ++) {
-        if (random(0,1) < 0.6) {
-            array.push(0);
-        } else {
+    for (var i = 0; i < vMapRows*vMapColumns; i ++) {
+        if (i%40===0 || i%40===39 || floor(i/40)===0 || floor(i/40)===39) {
             array.push(floor(random(1,4.999)));
+            
+        } else {
+            array.push((random(0,1) > 0.5) ? 0 : floor(random(1,4.999)));
         }
     }
     vMap = uint8.from(array);
-    
-    vMapRows = floor(vMap.length/vMapColumns);
+
     
 
    
@@ -120,10 +216,77 @@ var generateMap = function() {
     
 };
 
+var mapConnectionsScan = function() {
+    var tempArray = [];
+    for (var i = 0; i < vMap.length; i ++) {
+        tempArray.push(false);
+    }
+    var infectedMap = uint8.from(tempArray);
+    
+    infectedMap[vMap.length-1] = 1;
+    var endpoints = [vMap.length-1];
+
+    var maxTimes = 200;
+    var times = 0;
+    while (endpoints.length !== 0) {// infect solid tiles
+        for (var i = endpoints.length-1; i >= 0; i --) {
+            if (endpoints[i]%vMapColumns !== 0) {
+                var indexLeft = endpoints[i]-1;
+                if (vMap[indexLeft] && !infectedMap[indexLeft]) {
+                    infectedMap[indexLeft] = true;
+                    endpoints.push(indexLeft);
+                }
+    
+            } if (endpoints[i]%vMapColumns !== vMapColumns-1) {
+                var indexRight = endpoints[i]+1;
+                if (vMap[indexRight] && !infectedMap[indexRight]) {
+                    infectedMap[indexRight] = true;
+                    endpoints.push(indexRight);
+                }
+            }
+            if (endpoints[i] >= vMapColumns) {
+                var indexUp = endpoints[i]-vMapColumns;
+                if (vMap[indexUp] && !infectedMap[indexUp]) {
+                    infectedMap[indexUp] = true;
+                    endpoints.push(indexUp);
+                }
+            } 
+            if (endpoints[i] < vMap.length-vMapColumns) {
+                var indexDown = endpoints[i]+vMapColumns;
+                if (vMap[indexDown] && !infectedMap[indexDown]) {
+                    infectedMap[indexDown] = true;
+                    endpoints.push(indexDown);
+                }
+            }
+            
+            endpoints.splice(i, 1);
+        }
+        times ++;
+        if (times > maxTimes) {
+            break;
+        }
+
+    }
+    //non-infected tiles turned into particles
+    
+    for (var i = 0; i < vMap.length; i ++) {
+        if (vMap[i]) {
+            if (!infectedMap[i]) {
+                vMap[i] = 0;
+            }
+            
+        }
+    }
+    
+
+    
+    
+};
+
 var drawTiles = function() {
     var ts = cam.hw/cam.ht;
-    for (var layer = max(0, floor(RevY(0))); layer < min(floor(RevY(height))+1, vMapRows-1); layer ++) {
-        for (var col = max(0, floor(RevX(0))); col < min(floor(RevX(width ))+1, vMapColumns-1); col ++) {
+    for (var layer = max(0, floor(RevY(0))); layer < min(floor(RevY(height))+1, vMapRows); layer ++) {
+        for (var col = max(0, floor(RevX(0))); col < min(floor(RevX(width ))+1, vMapColumns); col ++) {
             fill(mapKeyColors[vMap[layer*vMapColumns+col]]);
             rect(X(col), Y(layer), S(1)+0.5, S(1)+0.5);
         }
@@ -239,37 +402,12 @@ var Player = function(x, y, w, h) {
     this.cameraTrackAheadAmount = 20;// how many frames ahead the camera predicts you will be *adjusted by camera height
     this.velX = 0;
     this.velY = 0;
-    this.speedX = 0.1;
-    this.speedY = 0.15;
+    this.speedX = 0.04;
+    this.speedY = 0.125;
     this.isFalling = true;// if not touching block
     this.collisionPushBack = 0.1;// in a perfect world, it would be exactly 0, but this allows the player to go between walls in this program
-    
-    this.onKeyDown = function(theCode) {
-        if (theCode === UP) {
-            if (!this.isFalling) {
-                this.upKeyPressed = true;
-                this.isFalling = true;
-                this.velY = -this.speedY;
-                
-            }
-        } else if (theCode === DOWN) {
-            
-        } else if (theCode === LEFT) {
-            this.velX = -this.speedX;
-        } else if (theCode === RIGHT) {
-            this.velX = this.speedX;
-        }
-        
-    };
-    
-    this.onKeyUp = function(theCode) {
-        if (theCode === UP) {
-            this.upKeyPressed = false;
-            
-        } else if (theCode === LEFT || theCode === RIGHT) {
-            this.velX = 0;
-        }
-    };
+    this.avatar = getImage("creatures/OhNoes");
+    this.isFacingLeft = true;
     
     this.configXs = function() {
         this.centerX = this.x1 + this.w/2;
@@ -321,85 +459,10 @@ var Player = function(x, y, w, h) {
             this.velY = (this.velY < maxGravityOnObject) ? this.velY + gravity : maxGravityOnObject;
         }
         
-        /* Tile collisions */
-        var onBlockIndexTL = vMap[floor(this.x1)+vMapColumns*floor(this.y1)];// top-left
-        if (onBlockIndexTL) {// top-left side of player///enable allllllllllllll///wii
-            var blockX = floor(this.x1)+0.5;
-            var blockY = floor(this.y1)+0.5;
-            if (abs(wasCenterX-blockX) > (abs(wasCenterY-blockY))) {///-1
-                this.velX = 0;
-                this.x1 = ceil(this.x1)+this.collisionPushBack; 
-                this.configXs();
-            } else {
-                this.velY = 0;
-                this.y1 = ceil(this.y1)+this.collisionPushBack; 
-                this.configYs();
-            }
-        } 
-        var onBlockIndexTR = vMap[floor(this.x2)+vMapColumns*floor(this.y1)];// top-right
-        if (onBlockIndexTR) {// right side of player
-            var blockX = floor(this.x2)+0.5;
-            var blockY = floor(this.y1)+0.5;
-            if (abs(wasCenterX-blockX) > abs(wasCenterY-blockY)) {
-                this.velX = 0;
-                this.x1 = floor(this.x2)-this.w-this.collisionPushBack; 
-                this.configXs();
-            } else {
-                this.velY = 0;
-                this.y1 = ceil(this.y1)+this.collisionPushBack; 
-                this.configYs();
-            }
-        } 
-        var onBlockIndexBR = vMap[floor(this.x2)+vMapColumns*floor(this.y2)];// bottom-right
         
-        if (onBlockIndexBR) {// bottom-right of player
-            var blockX = floor(this.x2)+0.5;
-            var blockY = floor(this.y2)+0.5;
-            if (abs(wasCenterX-blockX) > abs(wasCenterY-blockY)) {
-                this.velX = 0;
-                this.x1 = floor(this.x2)-this.w-this.collisionPushBack; 
-                this.configXs();
-            } else {
-                this.isFalling = false;
-                this.velY = 0;
-                this.y1 = floor(this.y2)-this.h-this.collisionPushBack; ///wait why am I referencing y2 instead of y1
-                this.configYs();
-            }
-        } 
-        var onBlockIndexBL = vMap[floor(this.x1)+vMapColumns*floor(this.y2)];// bottom-left
-        if (onBlockIndexBL) {// bottom-left side of player///collides TWICEOHHHH
-            var blockX = floor(this.x1)+0.5;
-            var blockY = floor(this.y2)+0.5;
-            if (abs(wasCenterX-blockX) > abs(wasCenterY-blockY)) {
-                this.velX = 0;
-                this.x1 = ceil(this.x1)+this.collisionPushBack; 
-                this.configXs();
-            } else {
-                this.isFalling = false;
-                this.velY = 0;
-                this.y1 = floor(this.y2)-this.h-this.collisionPushBack; 
-            }
-        }
-
+        collidelib.player_to_map(this);// map collisions
+   
         
-        if (!this.isFalling) {
-            var belowBlockIndexL = vMap[floor(this.x1)+vMapColumns*floor(this.y2+0.5)];// block below you (left side)
-            var belowBlockIndexR = vMap[floor(this.x2)+vMapColumns*floor(this.y2+0.5)];// block below you (right side)
-            if (!belowBlockIndexL && !belowBlockIndexR) {
-                 this.isFalling = true;
-            }
-        }
-        
-        
-        /*2* Top-left corner */
-        
-        
-        
-        /* UP key press logic 
-        if (!this.isFalling && this.upKeyPressed) {
-            this.isFalling = true;
-            this.velY = -this.speedY;
-        }*/
 
         
         /* Make camera follow player */
@@ -410,7 +473,43 @@ var Player = function(x, y, w, h) {
         cam.x=this.centerX;cam.y=this.centerY;
         
         /* Graphics */
-        fill(255, 0, 217);
+        //fill(255, 0, 217);
+        //rect(X(this.x1), Y(this.y1), S(this.w), S(this.h));
+        
+        
+        if (this.isFacingLeft) {
+            image(this.avatar, X(this.centerX), Y(this.centerY), S(this.w*1.3), S(this.h*1.3));
+            if (this.velX < 0) {
+                this.isFacingLeft = false;
+            }
+        } else {
+            translate(X(this.centerX), Y(this.centerY));
+            scale(-1, 1);
+            image(this.avatar, 0, 0, S(this.w*1.3), S(this.h*1.3));
+            resetMatrix();
+            if (this.velX > 0) {
+                this.isFacingLeft = true;
+            }
+        }
+    };
+    
+};
+
+var BigBlock = function(x, y, id256) {
+    this.x1 = x;
+    this.y1 = y;
+    this.w = 1;
+    this.h = 1;
+    this.x2 = x+this.w;
+    this.y2 = y+this.h;
+    this.id256 = id256;
+    
+    this.update = function() {
+        
+    };
+    
+    this.draw = function() {
+        fill(mapKeyColors[this.id256]);
         rect(X(this.x1), Y(this.y1), S(this.w), S(this.h));
     };
     
@@ -418,8 +517,10 @@ var Player = function(x, y, w, h) {
 
 /** Create instances **/
 var framework = new Framework();
-var player = new Player(5.5, 5, 0.5, 0.5);
+var player = new Player(vMapRows/2, vMapColumns/2, 0.2, 0.2);
 generateMap();
+
+mapConnectionsScan();
 
 /** Built-in functions **/
 mouseScrolled = function () {// Got great feedback from KWC@MKaelin368 to improve the scrolling method
